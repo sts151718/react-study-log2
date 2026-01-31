a1520b452686232be92b2ae3bfa0c36e91540559
sts151718 <tsubokurajob151718@gmail.com>
2026-01-31 14:48:26 +0900

    コメント追加

--- src/utils/supabase/superbaseStudyRecord.js
+++ src/utils/supabase/superbaseStudyRecord.js
@@ -1,12 +1,16 @@
 import { supabase } from './supabase';
 
+/**
+ * Supabaseから全データを取得
+ * @returns {{id: uuid, title: string, time: number}} 取得されたデータ
+ */
 export const fetchAllStudyRecords = async () => {
   const records = await supabase.from('study-record').select();
   return records;
 };
 
 /**
- * 1件のレコードを追加
+ * 1件分のレコードを追加
  * @param {Object} record 1件の学習記録
  * @param {string} record.title 学習内容
  * @param {number} record.time 学習時間
