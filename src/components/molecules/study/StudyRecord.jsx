849d3fcfac6a3e12690e16791b2366b0db45e187
sts151718 <tsubokurajob151718@gmail.com>
2026-01-31 14:35:14 +0900

    削除ボタン追加

--- src/components/molecules/study/StudyRecord.jsx
+++ src/components/molecules/study/StudyRecord.jsx
@@ -1,12 +1,21 @@
 import { memo } from 'react';
+import styled from 'styled-components';
+import { BaseButton } from '../../atoms/button/BaseButton';
 
 export const StudyRecord = memo((props) => {
   const { title, time } = props;
   return (
-    <>
-      <p key={title}>
+    <SContainer>
+      <span>
         {title} {time}時間
-      </p>
-    </>
+      </span>
+      <BaseButton>削除</BaseButton>
+    </SContainer>
   );
 });
+
+const SContainer = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 5px;
+`;
