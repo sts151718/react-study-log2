6180b60ea1ee1a9dd360fbc474c21afdb4448bbd
sts151718 <tsubokurajob151718@gmail.com>
2026-02-01 14:58:41 +0900

    jest設定 完了

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,14 @@
-import js from "@eslint/js";
-import globals from "globals";
-import reactHooks from "eslint-plugin-react-hooks";
-import reactRefresh from "eslint-plugin-react-refresh";
-import { defineConfig, globalIgnores } from "eslint/config";
-import eslintConfigPrettier from "eslint-config-prettier";
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import { defineConfig, globalIgnores } from 'eslint/config';
+import eslintConfigPrettier from 'eslint-config-prettier';
 
 export default defineConfig([
-  globalIgnores(["dist"]),
+  globalIgnores(['dist']),
   {
-    files: ["**/*.{js,jsx}"],
+    files: ['**/*.{js,jsx}'],
     extends: [
       js.configs.recommended,
       reactHooks.configs.flat.recommended,
@@ -19,13 +19,18 @@ export default defineConfig([
       ecmaVersion: 2020,
       globals: globals.browser,
       parserOptions: {
-        ecmaVersion: "latest",
+        ecmaVersion: 'latest',
         ecmaFeatures: { jsx: true },
-        sourceType: "module",
+        sourceType: 'module',
       },
     },
     rules: {
-      "no-unused-vars": ["error", { varsIgnorePattern: "^[A-Z_]" }],
+      // noopener noreferrer なしで target='_blank'を許可するか
+      'react/jsx-no-target-blank': 'off',
+      // コンポーネントと関数を同じファイルからエクスポートすることを許可するかどうか
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+      // 利用していない変数を許可するかどうか
+      'no-unused-vars': false,
     },
   },
 ]);
---
+++ jest.config.mjs
@@ -0,0 +1,15 @@
+/**
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+/** @type {import('jest').Config} */
+const config = {
+  testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    '\\.(css|less)$': 'identity-obj-proxy',
+  },
+  setupFilesAfterEnv: ['./jest.setup.js'],
+};
+
+export default config;
