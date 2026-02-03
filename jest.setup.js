9510e9b9754f204824104ebdd6a98fdedd0e61f3
sts151718 <tsubokurajob151718@gmail.com>
2026-02-03 12:15:15 +0900

    jest-preview, babel-preset-vite を導入

--- jest.setup.js
+++ jest.setup.js
@@ -1,4 +1,7 @@
 import '@testing-library/jest-dom';
+import { jestPreviewConfigure } from 'jest-preview';
 
 // dotenvの設定
 require('dotenv').config();
+
+jestPreviewConfigure({ autoPreview: false });
